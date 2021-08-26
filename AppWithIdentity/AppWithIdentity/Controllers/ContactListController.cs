d4cf2811db6c25d19e0b045ea7f36b2c87279852
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-08-26 19:50:58 +0300

    Initial project structure

---
+++ AppWithIdentity/AppWithIdentity/Controllers/ContactListController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWithIdentity.Controllers
+{
+    [Authorize]
+    public class ContactListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
